32a6a8226ee53c482ffc976db4ea00fd21ffc0ab
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2018-11-22 16:06:39 +0200

    Incapsulation done

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php 
+class LightSwitch {
+
+    private function on(){
+
+    }
+     private function off(){
+        
+    }
+     protected function connect(){
+        
+    }
+     private function activated(){
+        
+    }
+}
+
+$switch = new LightSwitch();
+$switch -> connect();
