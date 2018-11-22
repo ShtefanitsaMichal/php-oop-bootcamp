687d7896ceb90560738a60a1ea18e8be7e83e883
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2018-11-22 16:43:51 +0200

    abstract

---
+++ child_class.php
@@ -0,0 +1,40 @@
+<?php
+
+abstract class Shape{
+    protected $color;
+
+    public function __construct($color = "red")
+    {
+
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+
+class Square extends Shape{
+
+    protected $length = 4; 
+
+    public function GetArea(){
+        return pow($this -> length, 2);
+    }
+    
+}
+
+class Triangle extends Shape{
+
+    protected $base = 4;
+    protected $height = 7; 
+
+    public function GetArea(){
+       return .5*$this->base*$this->hight;
+    }
+    
+}
+
+var_dump(new Square())->getColor();
+ 
+ ?>
